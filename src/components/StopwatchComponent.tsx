9a9ca8b42f6d5ca88f06702245052258df491c68
Caio Oliveira <caio.vsoliveira23@gmail.com>
2025-05-18 22:46:10 -0300

    fix stopwatch hook

--- src/components/StopwatchComponent.tsx
+++ src/components/StopwatchComponent.tsx
@@ -1,16 +1,31 @@
+'use client'
+
+import { JSX } from "react";
 import { useStopwatch } from "@/hooks/useStopwatch"
 
-
 export function StopwatchComponent(): JSX.Element {
-	const stopW = useStopwatch();
+	const stopwatch = useStopwatch();
+
+	function handleStart() {
+		console.log('start');
+		stopwatch.start();
+	}
+
+	function handleStop() {
+		stopwatch.stop();
+	}
+
+	function handleReset() {
+		stopwatch.reset();
+	}
 
 	return (
 		<div>
-			<span>{}</span>
+			<span>{stopwatch.currentTimeFormatted()}</span>
 			<div>
-				<button onClick={}>Start</button>
-				<button onClick={}>Stop</button>
-				<button onClick={}>Reset</button>
+				<button onClick={handleStart}>Start</button>
+				<button onClick={handleStop}>Stop</button>
+				<button onClick={handleReset}>Reset</button>
 			</div>
 		</div>
 	)
--- src/hooks/useStopwatch.ts
+++ src/hooks/useStopwatch.ts
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 
-
 // Tracks elapsed time in seconds.
 // Provides functions to start, stop, and reset the stopwatch.
 // Returns the current time in seconds and a formatted minutes:seconds string.
@@ -41,9 +40,9 @@ export function useStopwatch(): UseStopwatchReturn {
 
 	function currentTimeFormatted() {
 		const currTime = currentTime()
-		const minutes = currTime / 60;
-		const seconds = currTime % 60;
-		return minutes.toString() + ':' + seconds.toString();
+		const minutes = (currTime / 60).toFixed();
+		const seconds = (currTime % 60).toFixed();
+		return minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0');
 	}
 
 	useEffect(() => {
@@ -52,16 +51,14 @@ export function useStopwatch(): UseStopwatchReturn {
 				return;
 			}
 
-			setTime((prev) => {
+			setTime(() => {
 				const elapse = Date.now() - playTimestamp;
-
-				// TODO: fix
-				return prev + elapse;
+				return elapse / 1000;
 			});
 		}, HALF_SECOND_IN_MILLIS)
 
 		return () => clearInterval(interv);
-	}, []);
+	}, [playTimestamp]);
 
 	return {
 		currentTime,
